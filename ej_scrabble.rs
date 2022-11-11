247b51717e3e411f3e55aa95b7d54052f669d969
Felipe <71887490+s0miez@users.noreply.github.com>
2022-11-11 01:31:29 -0300

    Create ej_scrabble.rs

---
+++ ej_scrabble.rs
@@ -0,0 +1,38 @@
+use std::io::stdin;
+fn main(){
+    println!("Ingrese texto: ");
+    let mut texto: String = String::new();
+    stdin().read_line(&mut texto).unwrap();
+    
+}
+
+fn valores() {
+    let a = 1;
+    let e = 1;
+    let i = 1;
+    let o = 1;
+    let u = 1;
+    let l = 1;
+    let n = 1;
+    let r = 1;
+    let s = 1;
+    let t = 1;
+    let d = 2;
+    let g = 2;
+    let b = 3;
+    let c = 3;
+    let m = 3;
+    let p = 3;
+    let f = 4;
+    let h = 4;
+    let v = 4;
+    let w = 4;
+    let y = 4;
+    let k = 5;
+    let j = 8;
+    let x = 8;
+    let q = 10;
+    let z = 10;
+    
+}
+
